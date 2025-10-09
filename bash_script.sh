06f94a3d2b35526b9664130a54c7abe791c45492
liviakm <livia.tahara.amaral@gmail.com>
2025-10-08 21:30:58 -0300

    Add files via upload

--- bash_script.sh
+++ bash_script.sh
@@ -1,9 +1,13 @@
+
 #!/bin/bash
 # Mostrar o diretório atual
 pwd
 
-# Gerar lista detalhada do conteúdo da pasta HOME e já redirecionar para lista_HOME.txt
+# Gerar lista detalhada do conteúdo da pasta HOME e redirecionar para lista_HOME.txt
 ls -la ~ > lista_HOME.txt
 
 # Imprimir a data atual na tela
 date
+
+
+
