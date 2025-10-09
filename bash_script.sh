5b426640640cdb21532d61182633c04aa0ac4b73
liviakm <livia.tahara.amaral@gmail.com>
2025-10-08 21:27:07 -0300

    Create bash_script.sh

---
+++ bash_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Mostrar o diretório atual
+pwd
+
+# Gerar lista detalhada do conteúdo da pasta HOME e já redirecionar para lista_HOME.txt
+ls -la ~ > lista_HOME.txt
+
+# Imprimir a data atual na tela
+date
